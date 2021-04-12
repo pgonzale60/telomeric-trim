033601dded4689f07af22efadf0a236f3da07842
pgonzale60 <pg17@sanger.ac.uk>
2021-04-12 15:00:01 +0000

    output reads begin on the side of the telomere

--- src/telomeric-trim.cr
+++ src/telomeric-trim.cr
@@ -1,13 +1,16 @@
 require "../lib/klib"
-
 include Klib
+
+# Trim telomeric repeats from reads
+# Reads containing telomere will be returned beggining with
+# the position where the telomere was found
 
 # default motif (nematodes)
 motif = "TTAGGC"
 min_occur = 3
 
 if ARGV.size < 1
-  puts "Usage: trim <in.fasta> [motif] [min_number_of_contiguous_occurrences]"
+  puts "Usage: telomeric-trim <in.fasta> [motif] [min_number_of_contiguous_occurrences]"
   puts ""
   exit(0)
 end
@@ -28,17 +31,22 @@ fp = GzipReader.new(ARGV[0])
 fx = FastxReader.new(fp)
 while (r = fx.read) >= 0
     read = fx.seq.to_s
+    # Search telomere
+    # in the end of the read in a space of 3 times the length of the motif
     if read.index(motif, -motif.size * 3)
         motifOccur = read.index(motif * min_occur)
         if motifOccur
             puts ">#{fx.name.to_s}"
-            puts read[0, motifOccur]
+            # Remove the telomere and reverse complement the read
+            puts read[0, motifOccur].reverse.tr("ATGC","TACG")
         end
+    # Search for reverse telomere in 
+    # the beggining of the read in a space of 3 times the length of the motif
     elsif read.rindex(rev_motif, motif.size * 3)
         motifOccur = read.rindex(rev_motif * min_occur)
         if motifOccur
             puts ">#{fx.name.to_s}"
-            puts read[motifOccur + rev_motif.size * min_occur, read.size].reverse.tr("ATGC","TACG")
+            puts read[motifOccur + rev_motif.size * min_occur, read.size]
         end
     end
 end
